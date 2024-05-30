0345717f9ca650e46ac636879356f1699e24e13c
LennardKrause <48315771+LennardKrause@users.noreply.github.com>
2024-05-30 11:11:09 +0200

    Add files via upload

--- xrdPlanner/__init__.py
+++ xrdPlanner/__init__.py
@@ -1,5 +1,6 @@
-__version__ = '1.8.0'
+__version__ = '1.8.1'
 __author__ = 'Lennard Krause'
+__authors__ = ['Lennard Krause', 'Frederik H. Gjørup', 'Mads R. V. Jørgensen']
 __email__ = 'lkrause@chem.au.dk'
 __date__ = '30.05.2024'
 __year__ = '2024'
